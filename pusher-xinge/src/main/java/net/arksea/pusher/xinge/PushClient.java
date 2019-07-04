3895042cdebd878d35e94d66667e7cd02dffc66e
xiaohaixing <arksea@gmail.com>
2019-07-04 13:50:19 +0800

    没有长连接，无需ping操作

--- pusher-xinge/src/main/java/net/arksea/pusher/xinge/PushClient.java
+++ pusher-xinge/src/main/java/net/arksea/pusher/xinge/PushClient.java
@@ -1,6 +1,7 @@
 package net.arksea.pusher.xinge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Platform;
 import net.arksea.pusher.IConnectionStatusListener;
 import net.arksea.pusher.IPushClient;
 import net.arksea.pusher.IPushStatusListener;
@@ -16,10 +17,12 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -29,7 +32,6 @@ import java.util.Map;
 public class PushClient implements IPushClient<String> {
     private final static Logger logger = LogManager.getLogger(PushClient.class);
     private final static ObjectMapper objectMapper = new ObjectMapper();
-    private long accessTokenExpiresTime;
     private final String postmanToken;
     private final String authorization;
     private final String PUSH_URL;
@@ -78,11 +80,14 @@ public class PushClient implements IPushClient<String> {
             post.addHeader("Authorization", authorization);
             post.addHeader("Cache-Control", "no-cache");
             post.addHeader("Postman-Token", postmanToken);
-            Map msgMap = objectMapper.readValue(event.payload, Map.class);
-            msgMap.put("audience_type", event.tokens.length > 1 ? "token_list" : "token");
-            msgMap.put("token_list", event.tokens);
-            String msg = objectMapper.writeValueAsString(msgMap);
-            HttpEntity entity = new StringEntity(msg, "UTF-8");
+            Map bodyMap = new HashMap();
+            bodyMap.put("audience_type", event.tokens.length > 1 ? "token_list" : "token");
+            bodyMap.put("token_list", event.tokens);
+            bodyMap.put("platform", "android");
+            bodyMap.put("message",  objectMapper.readValue(event.payload, Map.class));
+            bodyMap.put("message_type", "notify");
+            String postBody = objectMapper.writeValueAsString(bodyMap);
+            HttpEntity entity = new StringEntity(postBody, "UTF-8");
             post.setEntity(entity);
             CloseableHttpResponse response = httpclient.execute(post);
             int code = response.getStatusLine().getStatusCode();
@@ -131,23 +136,72 @@ public class PushClient implements IPushClient<String> {
 
     @Override
     public void ping(String session, IConnectionStatusListener listener) throws Exception {
-        logger.trace("PushClient.ping: {}", session);
-        long now = System.currentTimeMillis();
-        if (this.accessTokenExpiresTime - now < 600000) {
-            connect(listener);
-        }
     }
 
     @Override
     public boolean isAvailable(String session) {
-        long now = System.currentTimeMillis();
-        logger.trace("PushClient.isAvailable(), time={}", this.accessTokenExpiresTime - now);
-        return this.accessTokenExpiresTime - now > 300000;
+        return true;
     }
 
     @Override
     public void close(String session) {
-        logger.trace("PushClient.close()");
-        this.accessTokenExpiresTime = 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configurator.initialize("", "./push-server/src/test/resources/log4j2-test.xml");
+        IPushStatusListener pl = new IPushStatusListener() {
+            @Override
+            public void onPushSucceed(PushEvent event, int succeedCount) {
+                logger.info("推送成功");
+            }
+            @Override
+            public void onPushFailed(PushEvent event, int failedCount) {
+                logger.info("推送失败");
+            }
+            @Override
+            public void onRateLimit(PushEvent event) {
+                logger.info("推送失败");
+            }
+            @Override
+            public void handleInvalidToken(String token) {
+                logger.info("Invalid Token: {}", token);
+            }
+        };
+
+        final String[] tokens = new String[] {"6998779b7446c6974485b160e726c98ff61892ac"};
+        final String appId = "111";
+        final String appKey = "222";
+        final String postmanToken = "333";
+        final String payload = "{\"title\":\"2015-2019很可能是史上最热五年\",\"content\":\"极端高温事件频发，预计今年夏天北半球会出现更多热浪>>\",\"xg_media_resources\":\"http://bos.tq.ifjing.com/zxht/pic/20190701174005_17562.jpg\",\"android\":{\"action\":{\"action_type\":3,\"intent\":\"hlrlfl://b3BlbmFwcD9wYXJhbXM9aHR0cCUzQSUyRiUyRnRxLmlmamluZy5jb20lMkZzdGF0aWMlMkZhcHAlMkZ3ZWF0aGVyJTJGdG9waWMlMkZpbmZvLmh0bWwlM0ZhY3QlM0Q1MDUlMjZjQWN0JTNENCUyNmlkJTNEODYxMjE2MyUyNm1vZGVsJTNEc2Vhc29uJTI2aW5mb1RhZyUzRDEwMCUyNnNoYXJlVHlwZSUzRDQ=\"},\"custom_content\":{\"act\":\"http://tq.ifjing.com/static/app/weather/topic/info.html?act=505&cAct=4&id=8612163&model=season&infoTag=100&shareType=4\",\"stat\":{\"type\":7}}}}";
+        final PushClient client = new PushClient(appId, appKey, postmanToken);
+        PushEvent event = new PushEvent("1",
+            "20000105",
+            tokens,
+            payload,
+            "yunshi-test",
+            System.currentTimeMillis()+3600_000);
+
+        final IConnectionStatusListener cl = new IConnectionStatusListener() {
+            @Override
+            public void onSucceed() {
+                logger.info("onSucceed");
+            }
+            @Override
+            public void onFailed() {
+                logger.info("onFailed");
+            }
+            @Override
+            public void reconnect() {
+                logger.info("连接到推送服务器失败");
+            }
+            @Override
+            public void connected(Object session) {
+                logger.debug("connected: {}", session);
+                client.push("", event, this, pl);
+            }
+        };
+
+        client.connect(cl);
+        Thread.sleep(20000);
     }
 }
